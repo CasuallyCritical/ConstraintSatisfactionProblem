561c16920d783ca2d6f6ce3f4beb788e54e708d6
CasuallyCritical <onetruejdyt@gmail.com>
2022-11-11 14:14:38 -0500

    Updated with comments

--- Main.java
+++ Main.java
@@ -7,25 +7,35 @@ public class Main {
 	
 	ArrayList<Node> regions = new ArrayList<Node>();
 	
+	//Find solutions for generic k-coloring where k=some number of colors
 	public ArrayList<Node> findSolutions() {
 		Random rng = new Random();
+		//Start at a random region on the graph
 		int i = rng.nextInt(0, regions.size());
+		//create the queue
 		Queue<Node> queue = new LinkedList<Node>();
 		queue.add(regions.get(i));
 		
+		//Establish for backtracking
 		Node lastNode = regions.get(i);
 		
 		ArrayList<Node> colorPath = new ArrayList<Node>();
 		
+		//Begin out queue
 		while(queue.isEmpty() == false) {
 			Node n = queue.poll();
+			//We need to set color
 			if(n.color.isEmpty()) {
+				//this path failed, start over
 				if(n.domain.size() > 0) {
+					//Pick random color from domain
 					int c = rng.nextInt(n.domain.size());
 					String color = n.domain.get(c);
 					System.out.println(n.toString() + " Color: " + color);
 					
 					n.SetColor(color);
+					
+					//Does this color match any of our neighbors?
 					if(n.isFailCase() == false) {
 						
 						colorPath.add(n);
@@ -33,11 +43,13 @@ public class Main {
 						lastNode = n;
 						n.removeFromDomain(color);
 						
+						//Continue through the queue with our neighbors
 						for(Node neighbor : n.neighbors) {
 							queue.add(neighbor);
 							neighbor.removeFromDomain(color);
 						}
 					} else {
+						//Backtrack one step
 						n.SetColor(null);
 						queue.add(lastNode);
 					}
@@ -52,6 +64,7 @@ public class Main {
 	}
 	
 	public Main() {
+		//Using map of australia for this example, as it's a very common case
 		Node WA = new Node("Western Australia");
 		Node NT = new Node("Northern Territory");
 		Node SA = new Node("South Australia");
@@ -93,9 +106,12 @@ public class Main {
 		
 		ArrayList<Node> path = findSolutions();
 		
+		//If we failed, start over
 		if(path == null) {
 			new Main();
 		} else {
+			
+			//Display our final path
 			System.out.println("Final Path: " + '\n');
 			for(Node n : path) {
 				System.out.println("Region: " + n.toString());
